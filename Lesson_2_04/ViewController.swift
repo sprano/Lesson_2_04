a5475b1ad516d0120d216dba7843f6e2c42a0a84
Sofia <sofiapanoch@gmail.com>
2024-05-18 09:49:40 +0300

    Initial Commit

---
+++ Lesson_2_04/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_2_04
+//
+//  Created by Sofi Prano on 18.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
